a580ce9ecc2574b14ccd3377e01f329ab85c7124
sabin-scube <sabin@scube.co>
2025-10-05 09:43:33 +0530

    second commit

---
+++ command.js
@@ -0,0 +1,7 @@
+/**
+ * commands
+ *
+ * 1) git rm --cached index.js - it will bring staged files (from staging area) to unstaging area ( not tracking files)
+ *
+ * 2) git restore --staged filename or period (.) - for example: already file.js is in local repositary,now we have made some changes right now, now if we thought discard the changes which you made right now, that's time we can use this
+ */
