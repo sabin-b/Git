132242fffbfda740e15f7f2ff6538c0a5ffc613b
sabin-scube <sabin@scube.co>
2025-10-05 11:18:02 +0530

    git rest commands added

--- command.js
+++ command.js
@@ -3,5 +3,7 @@
  *
  * 1) git rm --cached index.js - it will bring staged files (from staging area) to unstaging area ( not tracking files)
  *
- * 2) git restore --staged filename or period (.) - for example: already file.js is in local repositary,now we have made some changes right now, now if we thought discard the changes which you made right now, that's time we can use this
+ * 2) git restore --staged filename or period (.) - for example: already file.js is in local repositary,now we have made some changes right now, files is in staging,  now if we thought discard the changes which you made right now, that's time we can use this
+ * 
+ * 3) git restore filename or or period (.) - now make sure file not in staging area, but that file already in local repo, you made some changes on the , you thought remove that changes , you can done with this command
  */
