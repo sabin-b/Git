5ad2d1b7d4ff2c9de3277bf0acefcf48014bd5b8
sabin-b <sabinb1998sabinb@gmail.com>
2025-10-05 12:26:46 +0530

    git revert command details added

--- command.js
+++ command.js
@@ -12,5 +12,15 @@
  * --soft - by default, command will work in soft mode, make sure soft mode when we do new files and changes will be exists, only head will be initial commit, now if we push, unitill then initial commit go to git repo
  * --hard - when we use this all of the files, folder and changes will be deleted, also head will be move to previous commit
  * 
- * 5) git revert commit-id - if we thought remove 
+ * 5) git revert commit-id - if we thought remove specfic file added that commit, that time we can use, suppose that time added new file along with made some changes previous it will merge conflict
+ * 
+ * options:
+ * --no-commit
+ * 
+ * scenario:
+ * Yes, your understanding is correct. If you added a new feature in a separate folder (for example, in "feature/"), and did not touch any older or unrelated files in that commit, and your later commits only changed other files (but not files in the "feature/" folder), then running git revert <commit-id> on the feature commit will only remove that feature folder and its contents.
+ The revert will not affect the other files you changed in later commits. There will be no merge conflicts, because no other commits after the feature commit modified the same files or folder. Only the addition of the feature will be undone; the rest of your codebase stays the same.
+ * 
+ * 
+ * 
  */
